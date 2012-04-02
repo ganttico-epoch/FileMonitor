903532e27c0c70ea8512bd8f6c625bf84e07e2df
Devico Beta <anonico.epoch@gmail.com>
2012-04-02 11:29:55 +0200

    Organizing project folders.

---
+++ input/java_app_src/file_monitor/Main.java
@@ -0,0 +1,25 @@
+package file_monitor;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Main {
+	
+	private static final Logger logger = Logger.getGlobal();
+	
+	public Main(String [] args) {
+		super();
+		Main.logger.info("Hello World!");
+	}
+
+	public static void main(String[] args) {
+		try {
+			Main.logger.setLevel(Level.parse(System.getProperty("FileMonitor.LOGLEVEL")));
+		} catch (Exception _) {
+			Main.logger.setLevel(Level.OFF);
+		}
+		new Main(args);
+		System.exit(0);
+	}
+
+}
