e7a6283276fbc3d2a263bafe34211f98587c4b09
Ganttico Epoch <ganttico.epoch@gmail.com>
2012-04-06 18:42:30 +0200

    Updated Main.

--- apps/FileMonitor-release/src_java/file_monitor/Main.java
+++ apps/FileMonitor-release/src_java/file_monitor/Main.java
@@ -1,25 +1,37 @@
 package file_monitor;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import javax.swing.ImageIcon;
+import javax.swing.JDialog;
+import javax.swing.JOptionPane;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
-	private static final Logger logger = Logger.getGlobal();
+	public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
 	public Main(String[] args) {
 		super();
-		Main.logger.info("Hello World !");
+		Main.LOGGER.info("Hello World!");
 	}
 
 	public static void main(String[] args) {
-		try {
-			Main.logger.setLevel(Level.parse(System
-					.getProperty("FileMonitor.LOGLEVEL")));
-		} catch (Exception _) {
-			Main.logger.setLevel(Level.ALL);
-		}
-		new Main(args);
+		/*
+		 * try { Main.logger.setLevel(Level.parse(System
+		 * .getProperty("FileMonitor.LOGLEVEL"))); } catch (Exception _) {
+		 * Main.logger.setLevel(Level.ALL); }
+		 */new Main(args);
+		JOptionPane pane = new JOptionPane("Monitoring current folder...",
+				JOptionPane.INFORMATION_MESSAGE);
+		pane.setOptions(new String [] {"Cancel"});
+		ImageIcon icon = new ImageIcon("icon.png");
+		pane.setIcon(icon);
+		
+		JDialog dialog = pane.createDialog(null, "File Monitoring");
+		dialog.setIconImage(icon.getImage());
+		dialog.pack();
+		dialog.setVisible(true);
 		System.exit(0);
 	}
 
